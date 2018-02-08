04fd7d4bc45dff70c88e4f40f0b74802aecf863e
Jeremiah Arella <jeremiah.arella@gmail.com>
2018-02-08 00:00:13 -0700

    Initializes project

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import { Fragment } from 'react';
+
+const HelloWorld = () => (
+  <Fragment>
+    <img src="/static/nextjs-logo.png"/>
+    <h1>Welcome to next.js!</h1>
+  </Fragment>
+)
+
+export default HelloWorld;

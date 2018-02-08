c058dfe31540cbcb8c429824bb8e8c1b7d0c8dcc
Jeremiah Arella <jeremiah.arella@gmail.com>
2018-02-08 01:28:47 -0700

    Adds a header to the about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,8 +1,13 @@
 import { Fragment } from 'react';
+import Head from 'next/head';
 import './styles.sass';
 
 const About = () => (
   <Fragment>
+    <Head>
+      <title>About</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <h1>This is the about page</h1>
   </Fragment>
 )
